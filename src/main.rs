9c4c0734e0ad37f202f3dfe18116527177397b96
Alireza-Ahmadi-x <118301277+Alireza-Ahmadi-x@users.noreply.github.com>
2023-02-08 14:28:35 -0500

    add function color (get text and change color

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// In The Name Of Allah
+
+#[allow(unused_imports)]
+use colored::Colorize;
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter a text");
+
+    let mut text = String::new();
+    io::stdin()
+        .read_line(&mut text)
+        .expect("Erro!");
+    
+    color(text);
+
+}
+
+
+fn color(x:String) -> (){
+    println!("{}",x.red());
 }
